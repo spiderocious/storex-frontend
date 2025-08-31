901444cc1aa0a1b42a4cd14d9fd14abf1e9ba128
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2025-08-31 02:58:28 +0100

    feat: add FileViewer component export in file-viewer index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
-// UI Components will be exported here
-// export { Button } from './button';
-// export { Input } from './input';
-// export { Card } from './card';
+export { Button } from './button';
+export { Input } from './input';
+export { Card } from './card';
+export { LoadingSpinner } from './loading-spinner';
 // export { Modal } from './modal';
+// export { ErrorBanner } from './error-banner';
